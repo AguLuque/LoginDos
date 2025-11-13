0d5f746b7677c12fc69f6188802916637cf813ed
Agustin <agustinluque.lbz@gmail.com>
2025-11-13 17:58:29 -0300

    Probando, preparadisimo

--- FrontLogin/my-app/src/Components/Login.jsx
+++ FrontLogin/my-app/src/Components/Login.jsx
@@ -21,7 +21,7 @@ const LoginForm = ({ onSwitchToRegister, onLogin }) => {
 
 
     try {
-      const response = await fetch(`${API_URL}/login`, {
+      const response = await fetch(`${apiurl}/login`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ email, password }),
--- FrontLogin/my-app/src/config/Const.jsx
+++ FrontLogin/my-app/src/config/Const.jsx
@@ -1,5 +1,5 @@
 const API_HOST = "http://192.168.68.123:3000";  // <-- CAMBIA ESTA IP POR LA TUYA REAL
 
-const API_URL = `${API_HOST}/api`;
+const apiurl = `${API_HOST}/api`;
 
-export default API_URL;
+export default apiurl;
